d61b21684a27c8c5794e166b6bcc2c35d2077a22
Алексей Зуев <>
2023-12-15 22:08:19 +0300

    Создал рыбу

---
+++ 4.Stack/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace _4.Stack;
+
+class Program
+{
+    static void Main()
+    {
+        var s = new Stack("a", "b", "c");
+        
+    }
+}
+
+public class Stack 
+{
+    public Stack(string a, string b, string c)
+    {
+        
+    }
+}
